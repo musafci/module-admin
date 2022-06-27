0da2d98d11315f4bf221c7f7e40e93c8ff729672
Md Abu Musa <musa.fci@gmail.com>
2022-06-27 15:56:23 +0600

    Create Upload Module

---
+++ app/Modules/Upload/Models/Upload.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Upload\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Upload extends Model
+{
+    use HasFactory;
+}
